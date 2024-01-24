19b6fbdb59fa56d55b2f7bfd4bdd23de0f656dbd
Spencer <spencerlelswick@gmail.com>
2024-01-23 23:54:59 -0500

    Adds modal show/hide functionality

--- src/app/shared/modal/modal.component.ts
+++ src/app/shared/modal/modal.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ModalService } from '../../services/modal.service';
 
 @Component({
   selector: 'app-modal',
   templateUrl: './modal.component.html',
-  styleUrl: './modal.component.css'
+  styleUrl: './modal.component.css',
 })
-export class ModalComponent {
+export class ModalComponent implements OnInit {
 
+
+  constructor(public modal: ModalService) { }
+
+  ngOnInit(): void {
+
+  }
+
+  closeModal() {
+    this.modal.toggleModal()
+  }
 }
