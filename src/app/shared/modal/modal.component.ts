24a0ba393cf52fe58c7aaad3075297e840b08532
Spencer <spencerlelswick@gmail.com>
2024-01-26 16:54:33 -0500

    Closes modal on login

--- src/app/shared/modal/modal.component.ts
+++ src/app/shared/modal/modal.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit, ElementRef } from '@angular/core';
+import { Component, Input, OnInit, ElementRef, OnDestroy } from '@angular/core';
 import { ModalService } from '../../services/modal.service';
 
 @Component({
@@ -6,7 +6,7 @@ import { ModalService } from '../../services/modal.service';
   templateUrl: './modal.component.html',
   styleUrl: './modal.component.css',
 })
-export class ModalComponent implements OnInit {
+export class ModalComponent implements OnInit, OnDestroy {
 
   @Input() modalID = ''
 
@@ -15,6 +15,10 @@ export class ModalComponent implements OnInit {
 
   ngOnInit(): void {
     document.body.appendChild(this.el.nativeElement)
+  }
+
+  ngOnDestroy(): void {
+    document.body.removeChild(this.el.nativeElement)
   }
 
   closeModal() {
--- src/app/user/login/login.component.ts
+++ src/app/user/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/compat/auth';
 
 @Component({
   selector: 'app-login',
@@ -7,13 +8,37 @@ import { Component } from '@angular/core';
 })
 export class LoginComponent {
 
+  constructor(private auth: AngularFireAuth) { }
+
   credentials = {
     email: '',
     password: ''
   }
+  showAlert = false;
+  alertMsg = ''
+  alertColor = ''
+  inSubmission = false
 
-  login() {
-    console.log(this.credentials);
+  async login() {
+    this.showAlert = true;
+    this.alertMsg = 'Please wait! You are being logged in...'
+    this.alertColor = 'blue'
+    this.inSubmission = true
+    try {
+      await this.auth.signInWithEmailAndPassword(
+        this.credentials.email, this.credentials.password
+      )
+    } catch (error) {
+      this.inSubmission = false
+      this.alertMsg = 'An unexpected error occured. Please try again later'
+      this.alertColor = 'red'
+
+      console.error(error);
+
+      return
+    }
+    this.alertMsg = 'Success! You are now logged in.'
+    this.alertColor = 'green'
   }
 
 }
