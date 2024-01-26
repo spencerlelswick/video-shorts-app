ebde5b7c2cb5e38527a19c83cb24169a203aa012
Spencer <spencerlelswick@gmail.com>
2024-01-26 17:42:49 -0500

    Adds logout

--- src/app/nav/nav.component.ts
+++ src/app/nav/nav.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { ModalService } from '../services/modal.service';
 import { AuthService } from '../services/auth.service';
+import { AngularFireAuth } from '@angular/fire/compat/auth';
 
 @Component({
   selector: 'app-nav',
@@ -11,15 +12,19 @@ export class NavComponent {
 
   public isAuthenticated = false
 
-  constructor(public modal: ModalService, public auth: AuthService) {
+  constructor(public modal: ModalService, public auth: AuthService, private afAuth: AngularFireAuth) {
     this.auth.isAuthenticated$.subscribe(status => {
       this.isAuthenticated = status
     })
   }
 
-
   openModal($event: Event) {
     $event.preventDefault()
     this.modal.toggleModal('auth')
+  }
+
+  async logout($event: Event) {
+    $event.preventDefault
+    await this.afAuth.signOut()
   }
 }
--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -3,7 +3,7 @@ import { AngularFireAuth } from '@angular/fire/compat/auth';
 import { AngularFirestore, AngularFirestoreCollection } from '@angular/fire/compat/firestore';
 import IUser from '../models/user.model';
 import { Observable } from 'rxjs';
-import { map } from 'rxjs/operators';
+import { delay, map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -12,12 +12,14 @@ export class AuthService {
 
   private usersCollection: AngularFirestoreCollection<IUser>
   public isAuthenticated$: Observable<boolean>
+  public isAuthenticatedWithDelay$: Observable<boolean>
 
   constructor(private auth: AngularFireAuth, private db: AngularFirestore) {
     this.usersCollection = db.collection('users')
     this.isAuthenticated$ = auth.user.pipe(
       map(user => !!user)
     )
+    this.isAuthenticatedWithDelay$ = this.isAuthenticated$.pipe(delay(1000))
   }
 
   public async createUser(userData: IUser) {
