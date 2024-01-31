3e1c76af056527b7448f897ecce0967e0753a408
Spencer <spencerlelswick@gmail.com>
2024-01-31 15:42:31 -0500

    Adds sub to route params for clip component

--- src/app/clip/clip.component.ts
+++ src/app/clip/clip.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Params } from '@angular/router';
 
 @Component({
   selector: 'app-clip',
   templateUrl: './clip.component.html',
   styleUrl: './clip.component.css'
 })
-export class ClipComponent {
+export class ClipComponent implements OnInit {
 
+  id = ''
+
+  constructor(public route: ActivatedRoute) { }
+
+  ngOnInit(): void {
+    this.route.params.subscribe((params: Params) => this.id = params?.['id'])
+  }
 }
