5c4bf33d17411320d918ac4966dad958764d9923
Spencer <spencerlelswick@gmail.com>
2024-02-01 07:25:36 -0500

    Updates video order by route params

--- src/app/video/manage/manage.component.ts
+++ src/app/video/manage/manage.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute, Params } from '@angular/router';
 
 @Component({
   selector: 'app-manage',
@@ -7,10 +8,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ManageComponent implements OnInit {
 
-  constructor() {
+  videoOrder = '1'
+
+  constructor(private router: Router, private route: ActivatedRoute) {
 
   }
 
   ngOnInit(): void {
+    this.route.queryParams.subscribe((params: Params) => {
+      this.videoOrder = params['sort'] === '2' ? params['sort'] : 1
+    })
+  }
+
+  sort(event: Event) {
+    const { value } = (event.target as HTMLSelectElement)
+    this.router.navigateByUrl(`/manage?sort=${value}`)
   }
 }
