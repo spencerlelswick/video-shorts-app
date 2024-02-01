6fc4beb72babf7d471c7e83907a32503155bcf99
Spencer <spencerlelswick@gmail.com>
2024-02-01 10:52:11 -0500

    Hides form until file is uploaded

--- src/app/video/upload/upload.component.ts
+++ src/app/video/upload/upload.component.ts
@@ -8,9 +8,16 @@ import { Component } from '@angular/core';
 export class UploadComponent {
 
   isDragover = false
+  file: File | null = null
+  hasUploadedFile = false
 
   storeFile($event: Event) {
     this.isDragover = false
+    this.file = ($event as DragEvent).dataTransfer?.files.item(0) ?? null
+    if (!this.file || this.file.type !== 'video/mp4') {
+      return
+    }
+    this.hasUploadedFile = true
   }
 
 }
