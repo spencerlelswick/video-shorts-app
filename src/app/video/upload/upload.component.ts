616a43e9c6a89e383c8d14302d45e3df4420c777
Spencer <spencerlelswick@gmail.com>
2024-02-02 08:40:44 -0500

    Adds timestamps to uploads

--- src/app/video/upload/upload.component.ts
+++ src/app/video/upload/upload.component.ts
@@ -93,7 +93,8 @@ export class UploadComponent implements OnDestroy {
           displayName: this.user?.displayName as string,
           title: this.title.value,
           fileName: `${clipFileName}.mp4`,
-          url: url
+          url: url,
+          timestamp: firebase.firestore.FieldValue.serverTimestamp()
         }
 
         const clipDocRef = await this.clipsService.createClip(clip)
